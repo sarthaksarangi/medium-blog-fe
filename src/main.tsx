48f072f6bc164a715ca9a9ca8fc74c081debf170
sarthaksarangi <sarthakrohan5@gmail.com>
2024-10-19 15:57:43 +0530

    Added skeleton for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signin from "./pages/Signin.tsx";
+import Signup from "./pages/Signup.tsx";
+import Blog from "./pages/Blog.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <App />,
+  },
+  {
+    path: "/signup",
+    element: <Signin />,
+  },
+  {
+    path: "/signin",
+    element: <Signup />,
+  },
+  {
+    path: "/blog/:id",
+    element: <Blog />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
